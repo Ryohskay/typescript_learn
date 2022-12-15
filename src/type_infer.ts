35eff688f231ecaf6a356a947fa3d871078b605d
Ryohskay on Pop <learnerryo@gmail.com>
2022-12-15 05:52:36 +0000

    added examples of code that does transpile while not compiled in most compiled languages

--- src/type_infer.ts
+++ src/type_infer.ts
@@ -2,4 +2,12 @@
 let msg = "hello there!";
 
 const upperMsg = msg.toUpperCase();
-const errorneous = msg/3;
+// Following invokes type error on transpilation as a string type value cannot be used in arithmetic operations.
+// const errorneous = msg/3;
+
+// Unfortunately, the following is compiled as ts is ultimately a javascript, a dynamically-typed language.
+//const possible = msg + 2;
+// this is also possible;
+//let number: number = 2;
+//const impossible = msg + number;  // string + number is defined in js, so this can be compiled ...
+
